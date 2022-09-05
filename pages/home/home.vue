a9ee5d649d336bfd4c8643cac967caf7f8775401
wgs66 <707205590@qq.com>
2022-09-05 11:42:36 +0800

    完成布局

---
+++ pages/home/home.vue
@@ -0,0 +1,40 @@
+<template>
+  <view>
+    <view class="topSearch">
+      <view class="search_btn">
+        搜索
+      </view>
+    </view>
+  </view>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        
+      };
+    }
+  }
+</script>
+
+<style lang="scss">
+.topSearch {
+  width: 750rpx;
+  height: 175rpx;
+  background-color: #c00000;
+  display: flex;
+  justify-content: center;
+  overflow: hidden;
+  .search_btn {
+    width: 500rpx;
+    height: 70rpx;
+    margin-top: 84rpx;
+    background-color: #fff;
+    border-radius: 20rpx;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+}
+</style>
